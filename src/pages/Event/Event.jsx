9a927a1c31bf33752038a74f2b8ce66b833bf112
bello hadi <bellohadi82@gmail.com>
2025-02-06 16:54:33 +0100

    :sparkles: adde the display grid and list feature

--- src/pages/Event/Event.jsx
+++ src/pages/Event/Event.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState } from "react";
 import { IoGrid } from "react-icons/io5";
 import { PiListChecks } from "react-icons/pi";
 import GridEvent from "../../Components/GridEvent/GridEvent";
 import ListEvent from "../../Components/ListEvent/ListEvent";
 const Event = ()=>{
+    const [ isGrid, setGrid ] = useState(true);
     return(
         <>
            <div className="w-[70vw] m-10">
@@ -14,16 +15,17 @@ const Event = ()=>{
                         Wedding, Lagos   ₦20k - ₦40k, Excellent
                     </p>
                     <div className="flex justify-evenly w-[100px]">
-                        <p className="text-3xl w-[30px] h-[30px] cursor-pointer">
+                        <p className="text-3xl w-[30px] h-[30px] cursor-pointer" onClick={()=> setGrid(true)}>
                             <IoGrid />
                         </p>
-                        <p className="flex items-center justify-evenly rounded-lg text-2xl border border-[#292D32] w-[30px] h-[30px] cursor-pointer">
+                        <p className="flex items-center justify-evenly rounded-lg text-2xl border border-[#292D32] w-[30px] h-[30px] cursor-pointer" onClick={()=> setGrid(!isGrid)}>
                             <PiListChecks />
                         </p>
                     </div>
                 </div>
-                {/* <GridEvent /> */}
-                <ListEvent />
+                {
+                    isGrid ? <GridEvent /> : <ListEvent />
+                }
            </div>
         </>
     );
